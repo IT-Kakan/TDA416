8d74e5a5ab353e827c26b618139261319cfdc192
Kakan-IT14 <jesper.olsson191@gmail.com>
2016-01-21 13:48:06 +0100

    created exercise methods

--- Lab1/src/MySqrt.java
+++ Lab1/src/MySqrt.java
@@ -1,5 +1,15 @@
 
 public class MySqrt {
+	
+	public static double mySqrtLoop(double x, double epsilon) {
+		return 0;
+
+	}
+
+	public static double mySqrtRecurse(double x, double epsilon) {
+		return 0;
+
+	}
 
 	/**
 	 * @param args
