7d5ffc512310316b255ab3433be09752f8ddec7e
Kakan-IT14 <jesper.olsson191@gmail.com>
2016-01-21 13:53:28 +0100

    added javadocs to the methods

--- Lab1/src/MySqrt.java
+++ Lab1/src/MySqrt.java
@@ -1,21 +1,29 @@
 
 public class MySqrt {
 	
+	/**
+	 * Iteratively calculates the square root of x.
+	 * @param x The value to take the square root of.
+	 * @param epsilon The allowed margin of error.
+	 * @return The sqaure root of x, approximated to within the margin of error.
+	 */
 	public static double mySqrtLoop(double x, double epsilon) {
 		return 0;
 
 	}
 
+	/**
+	 * Recursively calculates the square root of x.
+	 * @param x The value to take the square root of.
+	 * @param epsilon The allowed margin of error.
+	 * @return The sqaure root of x, approximated to within the margin of error.
+	 */
 	public static double mySqrtRecurse(double x, double epsilon) {
 		return 0;
 
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
