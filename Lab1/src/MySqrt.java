07805bbde6136287e1cb0eee9dda4729e1fdbc13
Kakan-IT14 <jesper.olsson191@gmail.com>
2016-01-21 15:14:01 +0100

    finished first iteration. Errors detected

--- Lab1/src/MySqrt.java
+++ Lab1/src/MySqrt.java
@@ -1,14 +1,40 @@
 
 public class MySqrt {
+	private static final double EPSILON = 10e-6;
 	
 	/**
 	 * Iteratively calculates the square root of x.
 	 * @param x The value to take the square root of.
 	 * @param epsilon The allowed margin of error.
-	 * @return The sqaure root of x, approximated to within the margin of error.
+	 * @return The square root of x, approximated to within the margin of error.
 	 */
 	public static double mySqrtLoop(double x, double epsilon) {
-		return 0;
+		double min;
+		double max;
+		
+		if (x < 0) {
+			return Double.NaN;
+		} else if (x <= 1) {
+			min = x;
+			max = 1;
+		} else {
+			min = 1;
+			max = x;
+		}
+		
+		double mid = (min + max)/2;
+		
+		while (Math.abs(mid * mid - x) >= epsilon) {
+			mid = (min + max)/2;
+			
+			if (mid * mid > x) {
+				max = mid;
+			} else {
+				min = mid;
+			}
+		}
+		
+		return mid;
 
 	}
 
@@ -16,15 +42,82 @@ public class MySqrt {
 	 * Recursively calculates the square root of x.
 	 * @param x The value to take the square root of.
 	 * @param epsilon The allowed margin of error.
-	 * @return The sqaure root of x, approximated to within the margin of error.
+	 * @return The square root of x, approximated to within the margin of error.
 	 */
 	public static double mySqrtRecurse(double x, double epsilon) {
-		return 0;
+		double min;
+		double max;
+		
+		if (x < 0) {
+			return Double.NaN;
+		} else if (x <= 1) {
+			min = x;
+			max = 1;
+		} else {
+			min = 1;
+			max = x;
+		}
+		
+		return mySqrtRecurse(x, epsilon, min, max);
 
+	}
+	
+	/**
+	 * Wrapper method for mySqrtRecurse
+	 * @param x The value to take the square root of.
+	 * @param epsilon The allowed margin of error.
+	 * @param min Lower bound of the interval which contains the correct value
+	 * @param max Upper bound of the interval which contains the correct value
+	 * @return The square root of x, approximated to within the margin of error.
+	 */
+	private static double mySqrtRecurse(double x, double epsilon, double min, double max) {
+		double mid = (min + max)/2;
+		double debug = Math.abs(mid * mid - x);
+		
+		if (Math.abs(mid * mid - x) < epsilon) {
+			return mid;
+		} else {
+			if (mid * mid > x) {
+				return mySqrtRecurse(x, epsilon, min, mid);
+			} else {
+				return mySqrtRecurse(x, epsilon, mid, max);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-
+		final double[] TEST_VALUES = {
+			//Test edge cases
+			0, 1,
+			//Test negative value
+			-10,
+			//Test known root in x > 1
+			81,
+			//Test known value in 0 < x < 1
+			0.81
+		};
+		
+		
+		for (double value : TEST_VALUES) {
+			double correctValue = Math.sqrt(value);
+			double loopValue = mySqrtLoop(value, EPSILON);
+			double recurseValue = mySqrtRecurse(value, EPSILON);
+			
+			System.out.println();
+			System.out.println();
+			System.out.println("Testing value " + value + ". Correct root is " + correctValue + ".");
+			
+			if (!(Math.abs(loopValue - correctValue) >= EPSILON)) {
+				System.out.println("mySqrtLoop passed, with result: " + loopValue);
+			} else {
+				System.out.println("########## mySqrtLoop failed, with result: " + loopValue);	
+			}
+			
+			if (!(Math.abs(recurseValue - correctValue) >= EPSILON)) {
+				System.out.println("mySqrtRecurse passed, with result: " + recurseValue);
+			} else {
+				System.out.println("########## mySqrtRecurse failed, with result: " + recurseValue);	
+			}
+		}
 	}
-
 }
