1924480cf9535a268ce0219c1d50ea327644a519
Kakan-IT14 <jesper.olsson191@gmail.com>
2016-01-28 13:47:07 +0100

    created method head for simplified shape

---
+++ Lab2/src/Lab2a.java
@@ -0,0 +1,26 @@
+
+public class Lab2a {
+
+	/**
+	 * Method to simplify a given shape by removing the least significant points until only k points remains.
+	 * Pre: poly contains coordinates such that poly[0] is the x-coordinate of the first point, poly[1] is the
+	 * y-coordinate of the first point, poly[2] is the x-coordinate of the second point and so on.
+	 * Pre: poly has a length of at least 4.
+	 * Pre: each element in poly is >= 0.
+	 * Post: poly is unaltered.
+	 * @param poly The points containing the shape to simplify.
+	 * @param k the number of points that the simplified shape should contain.
+	 * @return An array containing the simplified shape.
+	 */
+	public static double[] simplifyShape(double[] poly, int k) {
+		return null;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
