cf9b1663888249312545c84323ae75ce5447056e
Jesper Olsson <jesper.olsson191@gmail.com>
2016-02-09 11:11:06 +0100

    created concrete classes

---
+++ Lab3/src/SortedLinkedListSet.java
@@ -0,0 +1,29 @@
+
+public class SortedLinkedListSet<E extends Comparable<? super E>> implements SimpleSet<E> {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean add(E x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(E x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(E x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+}
---
+++ Lab3/src/SplayTreeSet.java
@@ -0,0 +1,28 @@
+
+public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<E>{
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean add(E x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(E x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(E x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
