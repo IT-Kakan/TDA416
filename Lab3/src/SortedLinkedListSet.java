c0bc8b52983035e0c5bbfb179e727c58a2ee7496
Jesper Olsson <jesper.olsson191@gmail.com>
2016-02-15 08:18:54 +0100

    completed, including testing, SortedLinkedListSet

--- Lab3/src/SortedLinkedListSet.java
+++ Lab3/src/SortedLinkedListSet.java
@@ -1,29 +1,97 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class SortedLinkedListSet<E extends Comparable<? super E>> implements SimpleSet<E> {
+	
+	LinkedList<E> sortedList = new LinkedList<>();
 
+
+	/**
+	 * Returns the number of elements in the set.
+	 * @return The number of elements in the set.
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return sortedList.size();
 	}
 
+	/**
+	 * Adds the element to the set. If the element already exists, nothing is altered.
+	 * @param x The element to be added.
+	 * @return True if the element did not exist. False otherwise.
+	 */
 	@Override
 	public boolean add(E x) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		//Only add an element if it is not already in the list.
+		if (contains(x)) {
+			return false;
+		} else {
+			
+			/*
+			//If the element is larger than the largest value in the list, insert the element last (time is O(1)).
+			if (x.compareTo(sortedList.getLast()) > 0) {
+				sortedList.addLast(x);
+				return true;
+			}
+			
+			//If the element is smaller than the smallest value in the list, insert the element first (time is O(1)).
+			if (x.compareTo(sortedList.getFirst()) < 0) {
+				sortedList.addFirst(x);
+				return true;
+			}	
+			
+			*/
+ 
+			
+			//Insert the element between the least lesser element and the least greater element.
+			//Captures the case where the element is less than all other elements.
+			ListIterator<E> iterator = sortedList.listIterator();
+			while (iterator.hasNext()) {
+				if (x.compareTo(iterator.next()) < 0) {
+					sortedList.add(iterator.previousIndex(), x);
+					return true;
+				}
+			}
+			//If the element is larger than all elements, insert last.
+			//Captures the case where the list is empty.
+			sortedList.addLast(x);
+			return true;
+		}
 	}
 
+	/**
+	 * Removes the element from the set. If it does not exist, nothing is altered.
+	 * @param x The element to remove.
+	 * @return True if the element was in the set. False otherwise.
+	 */
 	@Override
 	public boolean remove(E x) {
-		// TODO Auto-generated method stub
+
+		ListIterator<E> iterator = sortedList.listIterator();
+		while (iterator.hasNext()) {
+			if (x.compareTo(iterator.next()) == 0) {
+				sortedList.remove(iterator.previousIndex());
+				return true;
+			}
+		}
 		return false;
 	}
 
+	/**
+	 * Checks whether or not the element is part of the set.
+	 * @param x The element to be checked.
+	 * @return True is the element is part of the set. False otherwise.
+	 */
 	@Override
 	public boolean contains(E x) {
-		// TODO Auto-generated method stub
+		
+		ListIterator<E> iterator = sortedList.listIterator();
+		while (iterator.hasNext()) {
+			if (x.compareTo(iterator.next()) == 0) {
+				return true;
+			}
+		}
 		return false;
 	}
-
-	
 }
---
+++ Lab3/src/TestSetCorrectness.java
@@ -0,0 +1,114 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TestSetCorrectness {
+
+	public static void test(int implementationNbr, int nbrOfOperations, int nbrOfIntegers) {
+		
+		Random rand = new Random();
+		Set<Integer> reference = new TreeSet<>();
+		SimpleSet<Integer> implementation;
+		switch (implementationNbr) {
+		case 1:
+			implementation = new SortedLinkedListSet<>();
+			break;
+
+		case 2:
+			implementation = new SplayTreeSet<>();
+			break;
+			
+		default:
+			throw new IllegalArgumentException("Invalid implementation number.");
+		}
+		
+		for (int i = 0; i < nbrOfIntegers; i++) {
+			switch (rand.nextInt(4)) {
+			//Test size
+			case 0:
+				if (implementation.size() != reference.size()) {
+					System.out.println("Error in method size, implementation: " + implementation.getClass().toString());
+					System.out.println("Implementation size: " + implementation.size() + ". Reference size: " + reference.size());
+					System.exit(1);
+				}
+				break;
+
+			//Test add
+			case 1:
+				int toAdd = rand.nextInt(nbrOfIntegers);
+				boolean implementationAdd = implementation.add(toAdd);
+				boolean referenceAdd = reference.add(toAdd);
+				if (implementationAdd) {
+					//Assume contains is correct (is tested below)
+					if (!implementation.contains(toAdd)) {
+						System.out.println("Error in method add, implementation: " + implementation.getClass().toString());
+						System.out.println("Returned true but did not add element.");
+						System.exit(1);
+					}
+				}
+				
+				if (implementationAdd != referenceAdd) {
+					System.out.println("Error in method add, implementation: " + implementation.getClass().toString());
+					System.out.println("Value:" + toAdd);
+					System.out.println("Implementation add: " + implementationAdd + ". Reference add: " + referenceAdd);
+					System.exit(1);
+				}
+				break;
+				
+				
+			//Test remove
+			case 2:
+				int toRemove = rand.nextInt(nbrOfIntegers);
+				boolean implementationRemove = implementation.remove(toRemove);
+				boolean referenceRemove = reference.remove(toRemove);
+				
+				if (implementationRemove) {
+					if (implementation.contains(toRemove)) {
+						System.out.println("Error in method remove, implementation: " + implementation.getClass().toString());
+						System.out.println("Returned true but did not remove element.");
+						System.exit(1);
+					}
+				}
+				
+				if (implementationRemove != referenceRemove) {
+					System.out.println("Error in method remove, implementation: " + implementation.getClass().toString());
+					System.out.println("Value:" + toRemove);
+					System.out.println("Implementation remove: " + implementationRemove + ". Reference remove: " + referenceRemove);
+					System.exit(1);
+				}
+				break;
+				
+			//Test contains
+			case 3:
+				int toCheck = rand.nextInt(nbrOfIntegers);
+				boolean implementationContains= implementation.contains(toCheck);
+				boolean referenceContains = reference.contains(toCheck);
+				
+				if (implementationContains != referenceContains) {
+					System.out.println("Error in method contains, implementation: " + implementation.getClass().toString());
+					System.out.println("Value:" + toCheck);
+					System.out.println("Implementation contains: " + implementationContains + ". Reference contains: " + referenceContains);
+					System.exit(1);
+				}
+				break;
+				
+			default:
+				break;
+			} 
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		int implementationNbr = Integer.parseInt(args[0]);
+		int nbrOfRuns = Integer.parseInt(args[1]);
+		int nbrOfOperations = Integer.parseInt(args[2]);
+		int nbrOfIntegers = Integer.parseInt(args[3]);
+		for (int i = 0; i < nbrOfRuns; i++) {
+			test(implementationNbr, nbrOfOperations, nbrOfIntegers);
+		}
+		
+	}
+
+}
